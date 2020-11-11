3214f0f8c8b1267b271efc4ea2d2a85c4591eb0e
naloepk <nateloepker@gmail.com>
2020-11-11 13:09:35 -0600

    inital commit

---
+++ Assets/__Scripts/Enemy.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Set in Inspector: Enemy")]
+    public float speed = 10f; // The speed in m/s
+    public float fireRate = 0.3f; // Seconds/shot (Unused)
+    public float health = 10;
+    public int score = 100; // Points earned for destryoing this
+ 
+
+    private BoundsCheck bndCheck;
+
+    void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+
+    // This is a Property: A method that acts like a field 
+    public Vector3 pos
+    {
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        if(bndCheck != null && bndCheck.offDown)
+        {
+            // Check to make sure its gone off the bottom of the screen
+            if(pos.y < bndCheck.camHeight - bndCheck.radius)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        GameObject otherGO = coll.gameObject;
+        if(otherGO.tag == "ProjectileHero" || otherGO.tag == "ProjectileHero2")
+        {
+            Destroy(otherGO);
+            Destroy(gameObject);
+        }
+        else
+        {
+            print("Enemy hit by non-ProjectileHero: " + otherGO.name);
+        }
+    }
+}
---
+++ Assets/__Scripts/Hero2.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero2 : MonoBehaviour
+{
+    static public Hero2 S;
+    [Header("Set in Inspector")]
+    //These fields control the movement of the Ship
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+    public float gameRestartDelay = 2f;
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 40;
+
+    [Header("Set Dynamically")]
+    [SerializeField]
+    private float _shieldLevel = 1;
+
+
+
+
+    //This variable holds a reference to the alst triggering GameObject
+    private GameObject lastTriggerGo = null;
+
+    void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("Hero.Awake() - Attempted to assign second Hero.S!");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //pull information from the Input class
+        float xAxis = Input.GetAxis("Horizontal_2");
+        float yAxis = Input.GetAxis("Vertical_2");
+
+        //change transformation.position based on axes
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+
+        transform.position = pos;
+
+        //Rotate the ship to make it feel more dynamic 
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult + 90, xAxis * rollMult + 180, 0);
+
+        // Allow the ship to fire
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            TempFire();
+        }
+    }
+
+    void TempFire()
+    {
+        GameObject projGO = Instantiate<GameObject>(projectilePrefab);
+        projGO.transform.position = transform.position;
+        Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+        rigidB.velocity = Vector3.up * projectileSpeed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        //print("Triggered: " + go.name);
+
+        //Make sure its not the same triggering go as last time
+        if (go == lastTriggerGo)
+        {
+            return;
+        }
+        lastTriggerGo = go;
+        if (go.tag == "Enemy" || go.tag == "ProjectileHero")
+        {
+            shieldLevel--;
+            Destroy(go);
+
+        }
+        else
+        {
+            print("Triggered by non-Empy: " + go.name);
+        }
+
+
+    }
+    public float shieldLevel
+    {
+        get
+        {
+            return (_shieldLevel);
+        }
+        set
+        {
+            _shieldLevel = Mathf.Min(value, 4);
+            // If the shield is going to be set to less than zero 
+            if (value < 0)
+            {
+                Destroy(this.gameObject);
+                //Tell Main.S to restart the game after a delay
+                Main.S.DelayedRestart(gameRestartDelay);
+            }
+        }
+    }
+}
